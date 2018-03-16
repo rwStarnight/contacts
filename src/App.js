d74ee2a7c9f45a0f0a5d5551edb2bbba60091a23
rwStarnight <29952469+rwStarnight@users.noreply.github.com>
2018-03-16 15:02:30 -0400

    Finish the Contact Form and serialize the form data. this App is completed

--- src/App.js
+++ src/App.js
@@ -27,6 +27,14 @@ class App extends Component {
         ContactsAPI.remove(contact)
     }
 
+    createContact(contact) {
+        ContactsAPI.create(contact).then(contact => {
+            this.setState(state => ({
+                contacts: state.contacts.concat([ contact ])
+            }))
+        })
+    }
+
     render(){
       return (
         <div className="app">
@@ -36,7 +44,14 @@ class App extends Component {
                     onDeleteContact={this.removeContact}
                 />
             )}/>
-            <Route path="/create" component={CreateContact}/>
+            <Route path="/create" render={({ history }) => (
+                <CreateContact
+                onCreateContact={(contact) => {
+                    this.createContact(contact)
+                    history.push('/')
+                }}
+                />
+            )}/>
         </div>
       )
     }
